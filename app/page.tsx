70a54c344be2de95950091ad7a7f50f358d581ba
goskygo <softwarefordev@gmail.com>
2024-08-16 18:59:39 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  const showSession = () => {
+    if (status === "authenticated") {
+      return (
+        <button
+          className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          onClick={() => {
+            signOut({ redirect: false }).then(() => {
+              router.push("/");
+            });
+          }}
+        >
+          Sign Out
+        </button>
+      );
+    } else if (status === "loading") {
+      return <span className="text-[#888] text-sm mt-7">Loading...</span>;
+    } else {
+      return (
+        <Link
+          href="/login"
+          className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+        >
+          Sign In
+        </Link>
+      );
+    }
+  };
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-xl pb-4">Home</h1>
+      {showSession()}
+    </main>
+  );
+}
